f1e6f7eb7dedbfab25cb0788baf5f6a69d951bbd
tusin777 <ruslantusin@gmail.com>
2025-07-19 20:06:39 +0200

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import MyName from "../MyName/MyName";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Привет, React!</h1>
+      <p>Это мой первый React проект</p>
+      <MyName />
+      <MyName />
+      <MyName />
+    </>
+  );
+}
+
+export default App;
