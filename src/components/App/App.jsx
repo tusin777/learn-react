a89337edd3050de2f18d94215ec3edf3c9161f4e
tusin777 <ruslantusin@gmail.com>
2025-07-19 22:34:38 +0200

    fix: remove extra quote in element and update paragraph text in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,13 +4,13 @@ import "./App.css";
 
 function App() {
   const name = "Вася Пупкин";
-  const element = <h1>Алексей и {name} - друзья"</h1>;
+  const element = <h1>Алексей и {name} - друзья</h1>;
   const condition = true;
   const response = "<div>alert('Вы взломаны!')</div>";
   return (
     <>
       <h1>Привет, React!</h1>
-      <p>Это мой первый React проект</p>
+      <p>Это мой первый React-проект</p>
       {element}
       <div dangerouslySetInnerHTML={{ __html: response }}></div>
       <span>{3 + 8}</span>
